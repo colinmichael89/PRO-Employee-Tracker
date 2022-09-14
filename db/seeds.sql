3b2654ab69b6be4d0a3e4a39cdce6a44e97059ee
colinmichael89 <colinmichael89@gmail.com>
2022-09-14 17:41:25 -0600

    .env running with sequelize, dotenv, and bcrypt. Created connection.js file

---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+INSERT INTO department (department_name)
+VALUES ("Research and Development"), ("Sales"),
+("Marketing"), ("Accounting"),
+("Administration");
+
+INSERT INTO role (title, salary)
+VALUES ("Manager", 100000), 
+("Salesperson", 80000), 
+("Sales Lead", 120000), 
+("Lead Engineer", 90000), 
+("Software Engineer", 140000),
+("Employee", 90000), 
+("Manager", 100000),
+("Employee", 80000), 
+("Manager", 120000),
+("Employee", 90000), 
+("Manager", 150000),
+("Employee", 100000), 
+("Manager", 110000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("Joe", "Mama"), 
+("Johnny", "Cash"), 
+("Jimi", "Hendricks"), 
+("Colin", "McNatt"), 
+("Marty", "McFly"),
+("John", "Jacob"), 
+("Meghan", "Powell"),
+("Harley", "Quinn"), 
+("Jim", "James"),
+("Robert", "Plant"), 
+("Edward", "Scissorhands"),
+("Seymore", "Butts"), 
+("Sam", "Hill");
--- server.js
+++ server.js
@@ -1,32 +1,22 @@
 // Requirements
-// const sequelize = require("./config/connection");
 const express = require("express");
 const mysql = require("mysql2");
+const sequelize = require("./config/connection");
 const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express middleware
+// Express middleware to parse incoming JSON data
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to database
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    // MySQL username,
-    user: "root",
-    password: "Doobie#1!",
-    database: "employee_db",
-  },
-  console.log(`Connected to the employee_db database.`)
-);
-
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
 });
