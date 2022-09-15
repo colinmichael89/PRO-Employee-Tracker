e1a029b391aaa16539fd1f68edb8b99301750dd3
colinmichael89 <colinmichael89@gmail.com>
2022-09-14 21:43:48 -0600

    left join on query.sql

--- db/query.sql
+++ db/query.sql
@@ -1,6 +1,6 @@
-SELECT department.name AS department, FROM department JOIN name ON department.id = role.department_id;
+SELECT department.name AS department, FROM department LEFT JOIN name ON department.id = role.department_id;
 
-SELECT role.title AS title, FROM role JOIN role.title ON role.id = employee.role_id;
+SELECT role.title AS title, FROM role LEFT JOIN role.title ON role.id = employee.role_id;
 
-CONCAT(employee.first_name, ' ', employee.last_name) AS employee, FROM employee JOIN employee.employee ON employee.id = employee.manager_id;
+CONCAT(employee.first_name, ' ', employee.last_name) AS employee, FROM employee LEFT JOIN employee.employee ON employee.id = employee.manager_id;
 
